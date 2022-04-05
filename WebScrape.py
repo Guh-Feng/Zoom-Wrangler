1c062300d9318c2dfa1cda5d94c78b135b6eaf34
Ryan Lu <ryan.lu@ufl.edu>
2022-04-05 11:10:46 -0400

    Added code driver and tests, renamed WebScrape.py

--- WebScrape.py
+++ WebScrape.py
@@ -15,7 +15,10 @@ class Scraper:
         self.crawlSites = {}
         self.zoomLinks = []
         self.site = starterSite
-        self.browser = webdriver.Firefox()
+        # TODO: Add options for which browser to use.
+        #self.browser = webdriver.Firefox()
+        #self.browser = webdriver.Safari()
+        self.browser = webdriver.Chrome()
 
 
     #Check for valid link
@@ -26,6 +29,8 @@ class Scraper:
     def extractLinks(self, html):
         parse = bs4.BeautifulSoup(html, 'lxml')
         for link in parse.find_all('a', href = True):
+            # TODO: Add an exception that does not try to access links ending in
+            # common file extensions, like .pdf or .docx
             #If it is an external link, it is placed into a list to filter for zoom links
             url = urlparse(link.get('href'))
             if(url.scheme == 'https' and bool(url.netloc)):
@@ -59,6 +64,7 @@ class Scraper:
     def scrapeWeb(self):
         self.browser.get(self.site)
         while True:
+            # TODO: Be more clear with how the user should respond.
             userIn = input('\nHave you logged in?\n')
             if userIn == 'Y':
                 break
@@ -87,37 +93,10 @@ class Scraper:
         for link in links:
             if link not in self.crawlSites:
                 self.crawlSites[link] = False
+                
+    def returnLinks(self):
+        return self.zoomLinks
 
-def main():
-    #Test Sites
-    #https://ufl.instructure.com/courses/447867/assignments/syllabus
-    #https://ufl.instructure.com/courses/447867/pages/pm-slash-information?module_item_id=9127714
-    #https://ufl.instructure.com/courses/447867
-    #https://ufl.instructure.com/courses/447867/modules
-
-    inValue = ''
-
-#    try:
-    while True:
-        inValue = input('Enter website:')
-        userIn = input('\nIs this the correct website? Y/N\n')
-        if userIn == 'Y':
-            break
-        elif userIn == 'N':
-            continue
-        else:
-            continue
-
-    webScraper = Scraper(inValue)
-    webScraper.scrapeWeb()
-#    except Exception as exc:
-#        print('There was a problem: %s' % (exc))
-
-if __name__ == "__main__":
-    main()
-
-
-            
 
 #User navigates page and program gives option to scan the open page
 #Checks through pages, modules, and Zoom conferences tabs
