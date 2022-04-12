b2de46b58d365b60e3503f3ad943f335fec5a497
Ryan Lu <ryan.lu@ufl.edu>
2022-04-12 00:08:22 -0400

    Filters out download links

--- driver.py
+++ driver.py
@@ -1,8 +1,5 @@
 from WebScrape import Scraper
-import csv
-import time
-import webbrowser
-from datetime import datetime, date, timedelta
+from ReadWriteCSV import Database
 
 def main():
     #Test Sites
@@ -16,12 +13,10 @@ def main():
 #    try:
     while True:
         inValue = input('Enter website: ')
-        userIn = input('\nIs this the correct website? Y/N\n')
-        # TODO: Add support for lower-case Y/N and a message that
-        # tells the user that the input is invalid if not Y/N
-        if userIn == 'Y':
+        userIn = input('\nIs this the correct website? (Y/N)\n')
+        if userIn.capitalize() == 'Y':
             break
-        elif userIn == 'N':
+        elif userIn.capitalize() == 'N':
             continue
         else:
             continue
@@ -29,8 +24,21 @@ def main():
     webScraper = Scraper(inValue)
     webScraper.scrapeWeb()
     
-    zoomLinks = webScraper.returnLinks()
+    zoom_links = webScraper.returnLinks()
     
+    while True:
+        newness = input('\nWill you be creating a new file? (Y/N)\n')
+        if newness.capitalize() == 'Y':
+            newness = True
+            break
+        elif newness.capitalize() == 'N':
+            newness = False
+            break
+        else:
+            print('Invalid input!')
+            continue
+    
+    db = Database(zoom_links, is_new = newness)
     
 #    except Exception as exc:
 #        print('There was a problem: %s' % (exc))
