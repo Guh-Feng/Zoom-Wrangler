e75a28187a02b6151845b7b1b072efe64d02371a
Guh-Feng <58538080+Guh-Feng@users.noreply.github.com>
2022-04-12 00:08:26 -0400

    Functionality for User-Defined Pages Parsed

--- Web-Scrape.py
+++ Web-Scrape.py
@@ -5,26 +5,19 @@ import bs4
 import re
 import time
 
-#Functions
-#Finds all links in HTML
-#One for internal links and one for zoom links
-
-#Filters list of links for zoom links
+#Primary class
 class Scraper:
-    def __init__(self, starterSite):
+    def __init__(self, starterSite, starterCycles):
         self.crawlSites = {}
         self.zoomLinks = []
         self.site = starterSite
+        self.cycles = int(starterCycles)
         self.browser = webdriver.Firefox()
 
-
-    #Check for valid link
-    def is_valid(self, link):
-        parsed = urlparse(link)
-        return bool(parsed.netloc) and bool(parsed.scheme)
-
+    #Finds and filters links
     def extractLinks(self, html):
         parse = bs4.BeautifulSoup(html, 'lxml')
+        #Processes extracted links
         for link in parse.find_all('a', href = True, download = False):
             #If it is an external link, it is placed into a list to filter for zoom links
             url = urlparse(link.get('href'))
@@ -35,7 +28,7 @@ class Scraper:
                 if(urljoin(self.site, urlparse(link.get('href')).path) not in self.crawlSites):
                     self.crawlSites[urljoin(self.site, urlparse(link.get('href')).path)] = False
 
-    #Filters zoom links list to keep zoom links
+    #Filters out external links to keep Zoom links
     def zoomProcess(self):
         linkRegex = re.compile(r'ufl.zoom.us')
         iterator = 0
@@ -51,11 +44,13 @@ class Scraper:
                 listLen = len(self.zoomLinks)
         self.zoomLinks = list(set(self.zoomLinks))
 
+    #Prints Zoom list
     def printZoom(self):
         for links in self.zoomLinks:
             print(links)
             print('\n')
 
+    #Scrapes Canvas from a starting link
     def scrapeWeb(self):
         self.browser.get(self.site)
         while True:
@@ -68,58 +63,57 @@ class Scraper:
         self.extractLinks(html)
         self.zoomProcess()
 
-        while True:
-            try:
-                crawlIter = iter(self.crawlSites)
-                crawlSite = next(crawlIter) 
-                while self.crawlSites[crawlSite] == True:
-                    crawlSite = next(crawlIter)
-                self.browser.get(crawlSite)
-                self.crawlSites[crawlSite] = True
-                html = self.browser.page_source
-                self.extractLinks(html)
-                self.zoomProcess()
-            except StopIteration:
-                break
+        #If cycles is 0, it scrapes for as long as possible, otherwise, it scrapes for amount of pages provided by user
+        if(self.cycles):
+            for i in range(self.cycles):
+                try:
+                    crawlIter = iter(self.crawlSites)
+                    crawlSite = next(crawlIter) 
+                    while self.crawlSites[crawlSite] == True:
+                        crawlSite = next(crawlIter)
+                    self.browser.get(crawlSite)
+                    self.crawlSites[crawlSite] = True
+                    html = self.browser.page_source
+                    self.extractLinks(html)
+                    self.zoomProcess()
+                except StopIteration:
+                    break
+        else:
+            while True:
+                try:
+                    crawlIter = iter(self.crawlSites)
+                    crawlSite = next(crawlIter) 
+                    while self.crawlSites[crawlSite] == True:
+                        crawlSite = next(crawlIter)
+                    self.browser.get(crawlSite)
+                    self.crawlSites[crawlSite] = True
+                    html = self.browser.page_source
+                    self.extractLinks(html)
+                    self.zoomProcess()
+                except StopIteration:
+                    break
 
-    #Inputs links into the dictionary, make sure they are valid and filtered links
-    def linksDictionary(self, links):
-        for link in links:
-            if link not in self.crawlSites:
-                self.crawlSites[link] = False
+        self.printZoom()
 
+#Main function
 def main():
-    #Test Sites
-    #https://ufl.instructure.com/courses/447867/assignments/syllabus
-    #https://ufl.instructure.com/courses/447867/pages/pm-slash-information?module_item_id=9127714
-    #https://ufl.instructure.com/courses/447867
-    #https://ufl.instructure.com/courses/447867/modules
-
     inValue = ''
+    cyclesIn = 0
 
-#    try:
+    #Takes user inputs
     while True:
         inValue = input('Enter website:')
         userIn = input('\nIs this the correct website? Y/N\n')
         if userIn == 'Y':
+            cyclesIn = input('\nHow many pages do you want to search? Type 0 to search all available pages.\n')
             break
         elif userIn == 'N':
             continue
         else:
             continue
 
-    webScraper = Scraper(inValue)
+    webScraper = Scraper(inValue, cyclesIn)
     webScraper.scrapeWeb()
-#    except Exception as exc:
-#        print('There was a problem: %s' % (exc))
 
 if __name__ == "__main__":
     main()
-
-
-            
-
-#User navigates page and program gives option to scan the open page
-#Checks through pages, modules, and Zoom conferences tabs
-#Double hashtables via dictionaries, one for checked sites and one for unchecked sites for crawling pages
-#saveFile = open('site.txt', 'wb')
