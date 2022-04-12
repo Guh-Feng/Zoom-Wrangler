fc09ca636ef938c96f84d79dbf100a84b2ffa908
NickHobin <73544884+NickHobin@users.noreply.github.com>
2022-04-11 23:19:58 -0400

    Add files via upload

--- ui/tkinter test.py
+++ ui/tkinter test.py
@@ -1,120 +1,3 @@
-"""
-from tkinter import *
-from tkinter import ttk
-
-root = Tk()
-frm = ttk.Frame(root, padding=10)
-frm.grid()
-ttk.Label(frm, text="Hello World!").grid(column=0, row=0)
-ttk.Button(frm, text="Quit", command=root.destroy).grid(column=1, row=0)
-root.mainloop()
-"""
-
-
-
-"""
-import tkinter as tk
-from tkinter import filedialog, Text
-import os
-
-
-root = tk.Tk()
-apps = []
-
-if os.path.isfile('save.txt'):
-    with open('save.txt', 'r') as f:
-        tempApps = f.read()
-        tempApps = tempApps.split(',')
-        apps = [x for x in tempApps if x.strip()]
-        print(apps)
-
-
-def addApp():
-
-    for widget in frame.winfo_children():
-        widget.destroy()
-
-    filename = filedialog.askopenfilename(
-        initialdir="/", title="Select file", filetypes=(("executables", "*.exe"), ("all files", "*.*")))
-    apps.append(filename)
-    print(apps)
-    for app in apps:
-        label = tk.Label(frame, text=app, bg="gray")
-        label.pack()
-
-
-def runApps():
-    for app in apps:
-        os.startfile(app)
-
-
-canvas = tk.Canvas(root, height=700, width=700, bg="#263D42")
-canvas.pack()
-
-frame = tk.Frame(root, bg="white")
-frame.place(relx=0.1, rely=0.1, relwidth=0.8, relheight=0.8)
-
-openFile = tk.Button(root, text="Add Link", padx=25,
-                     pady=10, fg='white', bg="#263D42", command=addApp)
-openFile.pack()
-
-runApps = tk.Button(root, text="Open Meeting", padx=25,
-                    pady=10, fg='white', bg="#263D42", command=runApps)
-runApps.pack()
-
-for app in apps:
-    label = tk.Label(frame, text=app)
-    label.pack()
-
-
-root.mainloop()
-
-
-with open('save.txt', 'w') as f:
-    for app in apps:
-        f.write(app + ',')
-
-"""
-
-
-
-#textbox for input
-"""
-import tkinter as tk
-  
-# Top level window
-frame = tk.Tk()
-frame.title("TextBox Input")
-frame.geometry('400x200')
-# Function for getting Input
-# from textbox and printing it 
-# at label widget
-  
-def printInput():
-    inp = inputtxt.get(1.0, "end-1c")
-    lbl.config(text = "Provided Input: "+inp)
-  
-# TextBox Creation
-inputtxt = tk.Text(frame,
-                   height = 5,
-                   width = 20)
-  
-inputtxt.pack()
-  
-# Button Creation
-printButton = tk.Button(frame,
-                        text = "Print", 
-                        command = printInput)
-printButton.pack()
-  
-# Label Creation
-lbl = tk.Label(frame, text = "")
-lbl.pack()
-frame.mainloop()
-"""
-
-
-
 from tkinter import *
 import csv
 
@@ -130,18 +13,20 @@ myListbox.pack(pady=15)
 #add items from csv file
 myList = []
 
-with open("links.csv", "r", encoding='utf-8-sig') as csvfile:
-    reader = csv.reader(csvfile)
-    line_count = 0
-    stop_count = reader.line_num
-    for row in reader:
-        line_count = line_count + 1
-        if line_count == 1: #skip the header
-            continue
-        if line_count > stop_count: #stop at the end
-            break
-        print(row)
-        myList.append(row[6]) #get the link 
+#TODO: get links from csv file on startup
+"""
+reader = csv.reader(open("links.csv", "r", encoding='utf-8-sig'))
+line_count = 0
+stop_count = reader.line_num
+for row in reader:
+    line_count = line_count + 1
+    if line_count == 1: #skip the header
+        continue
+    if line_count > stop_count: #stop at the end
+        break
+    print(row)
+    myList.append(row[6]) #get the link 
+"""
 
 for item in myList:
     myListbox.insert(END, item)
