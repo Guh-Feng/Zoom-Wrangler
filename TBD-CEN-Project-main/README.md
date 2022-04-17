b2ccfbb8d3a81439bf9771d7847cf96de96e77b5
NickHobin <73544884+NickHobin@users.noreply.github.com>
2022-04-16 22:39:46 -0400

    Add files via upload

---
+++ TBD-CEN-Project-main/README.md
@@ -0,0 +1,42 @@
+# The Zoom Wrangler
+A program for those who can't bother to dig around for the right Zoom link for their appointments.
+
+## User dependencies:
+```
+pip install selenium beautifulsoup4 lxml plyer
+```
+
+## If using Chrome: requires ChromeDriver.
+### The chromedriver executable needs to be in PATH
+#### First, download the correct version of chromedriver that corresponds to your version of Chrome:
+```https://chromedriver.chromium.org/downloads```
+#### Then, unpackage the ZIP file and add the resulting binary to your PATH
+##### This will vary by machine. One could run ```echo $PATH``` in terminal to view the directories that are in the PATH environment variable. 
+#### Put the binary in one of these directories or run this command in terminal:
+
+```export PATH=[INSERT_DIRECTORY_HERE]:$PATH```
+
+#### where ```[INSERT_DIRECTORY_HERE]``` is replaced with the complete path to ```chromedriver```
+
+#### For example:
+```export PATH=/Users/username/Documents/driverfolder:$PATH```
+
+##### NOTE: It is crucial that ```:$PATH``` is appended to this command, otherwise your PATH environment variable may be irreversibly modified.
+#### Then, restart your machine.
+### Developer tests require ```pytest```
+
+## Additional information for MacOS & Safari (please read instructions carefully and completely before proceeding):
+
+### The 'Allow Remote Automation' option in Safari's Develop Menu must be enabled for Selenium compatability.
+### Safari's Develop menu must be enabled for access in the Menu Bar.
+#### This can be enabled through Preferences -> Advanced -> Show Develop menu in menu bar.
+### Enable the 'Allow Remote Automation' option under the Develop menu.
+#### Some devices may have to scroll within the Develop menu to see this option.
+
+### Safari has to enable WebDriver support
+#### For High Sierra (10.13) and later, run this command in terminal: ```safaridriver --enable```
+
+#### NOTE: If upgraded from a previous MacOS release, the ```sudo``` command may need to be used.
+
+### For Sierra (10.12) and earlier, you must authorize ```safaridriver``` to launch the XPC service that hosts the local web server.
+#### Manually run ```/usr/bin/safaridriver``` in terminal and follow the authentication prompt.
---
+++ TBD-CEN-Project-main/ui/ReadWriteCSV.py
@@ -0,0 +1,68 @@
+import csv
+import webbrowser
+from datetime import datetime, date, timedelta
+
+class Database:
+    def __init__(self, passed_links, is_new = True):
+        self.file_name = ''
+        if is_new:
+            self.file_name = input('Please input name of the new database file: ')
+        else:
+            self.file_name = input('Please input name of the database file: ')
+        self.file_name = self.file_name + '.csv'
+        with open(self.file_name, 'a', newline='') as csvfile:
+            writer = csv.writer(csvfile)
+            if is_new:
+                writer.writerow(['Class Code', 'Class Name', 'Day of week (0 = Monday, 1 = Tuesday..)',
+                                'Notification Time', 'Auto-Open Time', 'Start Time', 'Link'])
+            print('Found', len(passed_links), 'link(s)!')
+            for link in passed_links:
+                print('\nFound Zoom link:', link)
+                looping = True
+                while looping:
+                    choice = input("\nContinue adding this link to database? (Y/N) ")
+                    if choice.capitalize() == 'N':
+                        looping = False
+                        break
+                    elif choice.capitalize() == 'Y':
+                        # Get user input about this appointment
+                        row = []
+                        row.append(input("Class Code: "))
+                        row.append(input("Class Name: "))
+                        row.append(input("Day of week (0 = Monday, 1 = Tuesday..): "))
+                        start_time = input("Start Time (HH:MM): ")
+                        mins_before_notif = input("How many minutes before the start time should we send a notification? ")
+                        mins_before_auto_open = input("How many minutes before the start time should we auto-open the link? ")
+                        
+                        # Create datetime objects, can easily maniupulate using timedelta
+                        dt_start_time = datetime.fromisoformat(date.today().isoformat() + ' ' + start_time)
+                        dt_notif_time = dt_start_time - timedelta(hours = 0, minutes = int(mins_before_notif))
+                        dt_auto_open_time = dt_start_time - timedelta(hours = 0, minutes = int(mins_before_auto_open))
+                        
+                        # Strings to put into database for storage
+                        db_start_time = dt_start_time.isoformat(timespec = 'minutes').replace(date.today().isoformat(), '')
+                        db_notif_time = dt_notif_time.isoformat(timespec = 'minutes').replace(date.today().isoformat(), '')
+                        db_auto_open_time = dt_auto_open_time.isoformat(timespec='minutes').replace(date.today().isoformat(), '')
+                        
+                        row.append(db_notif_time)
+                        row.append(db_auto_open_time)
+                        row.append(db_start_time)
+                        row.append(link)
+                        writer.writerow(row)
+                        break
+                    else:
+                        print('Invalid input!')
+                        continue
+    
+    def fetch(self, row_count):
+        with open(self.file_name, "r", encoding='utf-8-sig') as csvfile:
+            reader = csv.reader(csvfile)
+            line_count = 0
+            stop_count = row_count
+            for row in reader:
+                line_count = line_count + 1
+                if line_count == 1:
+                    continue
+                if line_count > stop_count:
+                    break
+            return row
